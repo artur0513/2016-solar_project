515aa189a7341baaf2889f24990f8c50904bbc47
hadyzhensky <91096898+hadyzhensky@users.noreply.github.com>
2021-11-30 23:51:30 +0300

    Update solar_input.py

--- solar_input.py
+++ solar_input.py
@@ -1,8 +1,7 @@
-# coding: utf-8
 # license: GPLv3
 
 from solar_objects import Star, Planet
-from matplotlib import pyplot as plt
+
 
 def read_space_objects_data_from_file(input_filename):
     """Cчитывает данные о космических объектах из файла, создаёт сами объекты
@@ -12,22 +11,22 @@ def read_space_objects_data_from_file(input_filename):
     """
 
     objects = []
-    if input_filename != '':
-        with open(input_filename) as input_file:
-            for line in input_file:
-                if len(line.strip()) == 0 or line[0] == '#':
-                    continue  # пустые строки и строки-комментарии пропускаем
-                object_type = line.split()[0].lower()
-                if object_type == "star":
-                    star = Star()
-                    parse_star_parameters(line, star)
-                    objects.append(star)
-                elif object_type == "planet":
-                    planet = Planet()
-                    parse_planet_parameters(line, planet)
-                    objects.append(planet)
-                else:
-                    print("Unknown space object")
+    with open(input_filename) as input_file:
+        for line in input_file:
+            if len(line.strip()) == 0 or line[0] == '#':
+                continue  # пустые строки и строки-комментарии пропускаем
+            object_type = line.split()[0].lower()
+            if object_type == "star":  # FIXME: do the same for planet
+                star = Star()
+                parse_star_parameters(line, star)
+                objects.append(star)
+            elif object_type == "planet":  # FIXME: do the same for planet
+                star = Planet()
+                parse_planet_parameters(line, star)
+                objects.append(star)
+            else:
+                print("Unknown space object")
+
     return objects
 
 
@@ -42,15 +41,12 @@ def parse_star_parameters(line, star):
     **line** — строка с описание звезды.
     **star** — объект звезды.
     """
-
-    tokens = line.split()
-    star.R = int(tokens[1])
-    star.color = tokens[2]
-    star.m = float(tokens[3])
-    star.x = float(tokens[4])
-    star.y = float(tokens[5])
-    star.Vx = float(tokens[6])
-    star.Vy = float(tokens[7])
+    params = line.split(" ")
+    star.R = float(params[1])
+    star.color = params[2]
+    star.m = float(params[3])
+    star.x, star.y = float(params[4]), float(params[5])
+    star.Vx, star.Vy = float(params[6]), float(params[7])
 
 
 def parse_planet_parameters(line, planet):
@@ -65,15 +61,12 @@ def parse_planet_parameters(line, planet):
     **line** — строка с описание планеты.
     **planet** — объект планеты.
     """
-
-    tokens = line.split()
-    planet.R = int(tokens[1])
-    planet.color = tokens[2]
-    planet.m = float(tokens[3])
-    planet.x = float(tokens[4])
-    planet.y = float(tokens[5])
-    planet.Vx = float(tokens[6])
-    planet.Vy = float(tokens[7])
+    params = line.split(" ")
+    planet.R = float(params[1])
+    planet.color = params[2]
+    planet.m = float(params[3])
+    planet.x, planet.y = float(params[4]), float(params[5])
+    planet.Vx, planet.Vy = float(params[6]), float(params[7])
 
 
 def write_space_objects_data_to_file(output_filename, space_objects):
@@ -85,7 +78,7 @@ def write_space_objects_data_to_file(output_filename, space_objects):
     **output_filename** — имя входного файла
     **space_objects** — список объектов планет и звёзд
     """
-    if output_filename != '':
+  if output_filename != '':
         with open(output_filename + '.txt', 'w') as file:
             for obj in space_objects:
                 string = str(obj.type) + ' ' + str(obj.R) + ' ' + str(obj.color) + ' ' + str(obj.m) + \
@@ -203,7 +196,7 @@ def draw_plots(legend, t, r, v):
     plt.title(r'$r(t)$')
     plt.grid(True)
     plt.show()
-    
+
     plt.plot(r, v)
     plt.title(r'$V(r)$')
     plt.grid(True)
